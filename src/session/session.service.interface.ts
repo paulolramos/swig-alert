189f764ba2dcf95e2c4e36c92d254ee03c4770b1
Paulo Ramos <PauloRamos64@gmail.com>
2021-01-17 21:49:07 -0800

    update service interfaces

--- src/session/session.service.interface.ts
+++ src/session/session.service.interface.ts
@@ -2,7 +2,12 @@ import { Session } from 'src/session/session.entity';
 
 export interface ISessionService {
   getAllSessionsByUserId: (userId: string) => Promise<Session[]>;
+  getAllInactiveSessionsByUserId: (userId: string) => Promise<Session[]>;
   getSessionById: (userId: string, id: string) => Promise<Session>;
+  getCurrentSession: (userId: string) => Promise<Session>;
+  hasCurrentSession: (
+    userId: string,
+  ) => Promise<{ hasActiveSession: boolean; sessionId: number }>;
   createSession: (userId: string, session: Session) => Promise<Session>;
   deleteSession: (userId: string, id: string) => Promise<void>;
   endSession: (userId: string, id: string) => Promise<void>;
--- src/user/user.service.interface.ts
+++ src/user/user.service.interface.ts
@@ -2,8 +2,9 @@ import { User } from 'src/user/user.entity';
 
 export interface IUserService {
   getAllUsers: () => Promise<User[]>;
-  findUserById: (id: string) => Promise<User>;
   findUserByUsername: (username: string) => Promise<User>;
+  findUserByEmail: (email: string) => Promise<User>;
+  findUserById: (id: string) => Promise<User>;
   getFullUserProfile: (id: string) => Promise<User>;
   createUser: (user: User) => Promise<User>;
   updateUser: (id: string, user: User) => Promise<boolean>;
